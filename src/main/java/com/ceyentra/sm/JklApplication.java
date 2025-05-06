b905d3ce9e8e66ab49f7cb26c5504b0c46c612ed
N. Sandunsara <sandunsara2002@gmail.com>
2025-05-06 16:13:11 +0530

    add frist commit with poject structure

---
+++ src/main/java/com/ceyentra/sm/JklApplication.java
@@ -0,0 +1,29 @@
+package com.ceyentra.sm;
+
+import com.ceyentra.sm.config.EnvConfigLoader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableAsync
+@EnableTransactionManagement
+@EnableScheduling
+public class JklApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JklApplication.class);
+	}
+
+	public static void main(String[] args){
+		// Load .env file
+		//EnvConfigLoader.loadEnv();
+		SpringApplication.run(JklApplication.class, args);
+	}
+
+}
